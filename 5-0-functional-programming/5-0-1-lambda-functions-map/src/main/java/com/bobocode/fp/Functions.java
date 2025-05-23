1fb8e7fd70faf24f0e443ca8d918a51a6d08edc2
unknown <abdulazizabduganiev4@gmail.com>
2025-05-23 12:18:26 +0500

    solved exercise 5-0-1

--- 5-0-functional-programming/5-0-1-lambda-functions-map/src/main/java/com/bobocode/fp/Functions.java
+++ 5-0-functional-programming/5-0-1-lambda-functions-map/src/main/java/com/bobocode/fp/Functions.java
@@ -28,7 +28,12 @@ public class Functions {
     public static FunctionMap<Integer, Integer> intFunctionMap() {
         FunctionMap<Integer, Integer> intFunctionMap = new FunctionMap<>();
 
-        // todo: according to the javadoc add functions using lambda expression
+        intFunctionMap.addFunction("abs", x -> Math.abs(x));
+        intFunctionMap.addFunction("sgn", x -> x != 0 ? x / Math.abs(x) : 0);
+        intFunctionMap.addFunction("increment", x -> x + 1);
+        intFunctionMap.addFunction("decrement", x -> x - 1);
+        intFunctionMap.addFunction("square", x -> x * x);
+
 
         return intFunctionMap;
     }
